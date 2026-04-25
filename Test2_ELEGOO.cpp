4b8e8198b4fc23a5f2e1e6ac11e3790f79aaacc7
Tina314159 <127436727+Tina314159@users.noreply.github.com>
2026-04-24 18:38:06 -0700

    add ELEGOO trigger pin file

---
+++ Test2_ELEGOO.cpp
@@ -0,0 +1,19 @@
+const int TRIGGER_PIN = 2; // Hardware interrupt pin
+volatile bool triggered = false;
+
+void onTrigger() {
+  triggered = true;
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(TRIGGER_PIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(TRIGGER_PIN), onTrigger, RISING);
+}
+
+void loop() {
+  if (triggered) {
+    triggered = false;
+    Serial.println("START");  // Python listens for this
+  }
+}
