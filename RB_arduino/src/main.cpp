c1da73d927e1fd4274e504d4627dbbd80d6f0b1b
Tina314159 <127436727+Tina314159@users.noreply.github.com>
2026-04-24 20:20:22 -0700

    Initialize and control LED in main loop
    Added LED_BUILTIN initialization and control in loop.

--- RB_arduino/src/main.cpp
+++ RB_arduino/src/main.cpp
@@ -5,13 +5,17 @@ int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  int result = myFunction(2, 3); 
+  pinMode(LED_BUILTIN, OUTPUT); // Initialize digital pin LED_BUILTIN as an output
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, HIGH);   // Turn the LED on
+  delay(1000);                       // Wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // Turn the LED off
+  delay(1000);                       // Wait for a second
 }
-
 // put function definitions here:
 int myFunction(int x, int y) {
   return x + y;
