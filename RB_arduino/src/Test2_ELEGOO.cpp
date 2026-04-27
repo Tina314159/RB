849a828ab5240f6892449aa5e5970225b0a4c6c4
TH00 <tianqihu@uw.edu>
2026-04-27 14:25:10 -0700

    add LED blink indication

--- RB_arduino/src/Test2_ELEGOO.cpp
+++ RB_arduino/src/Test2_ELEGOO.cpp
@@ -2,6 +2,8 @@
 
 const int TRIGGER_PIN = 2; // Hardware interrupt pin
 volatile bool triggered = false;
+const int LED_indicator = 1; 
+    //1 = LED will be used for indication (introcude delay)
 
 void onTrigger() {
   triggered = true;
@@ -10,6 +12,8 @@ void onTrigger() {
 void setup() {
   Serial.begin(115200);
   pinMode(TRIGGER_PIN, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT); // Initialize digital pin LED_BUILTIN as an output 
+  digitalWrite(LED_BUILTIN, HIGH);  
   attachInterrupt(digitalPinToInterrupt(TRIGGER_PIN), onTrigger, RISING);
 }
 
@@ -17,5 +21,12 @@ void loop() {
   if (triggered) {
     triggered = false;
     Serial.println("START");  // Python listens for this
+    if (LED_indicator == 1) {
+      digitalWrite(LED_BUILTIN, HIGH);   // Turn the LED on
+      delay(1000);                       // Wait for a second
+      digitalWrite(LED_BUILTIN, LOW);    // Turn the LED off
+      delay(1000);
+      digitalWrite(LED_BUILTIN, HIGH); 
+    }
   }
 }
